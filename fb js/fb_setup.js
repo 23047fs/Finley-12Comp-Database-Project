c4a5d52c74e363ac75aa7472be48d3670150b1ad
23047fs <23047fs@hvhs.school.nz>
2026-06-04 10:36:23 +1200

    setting up

---
+++ fb js/fb_setup.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcR-wxDz2JJXUlqbfOkgofE4nDxqueMvI",
+  authDomain: "comp-project-28c74.firebaseapp.com",
+  databaseURL: "https://comp-project-28c74-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "comp-project-28c74",
+  storageBucket: "comp-project-28c74.firebasestorage.app",
+  messagingSenderId: "561132125335",
+  appId: "1:561132125335:web:70cc4276e31eb2871dbdd2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
