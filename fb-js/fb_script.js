577e3ae00f8ed03ed93ff7f12c4353d594e10dbd
23047fs <23047fs@hvhs.school.nz>
2026-06-23 15:08:29 +1200

    space

--- fb-js/fb_script.js
+++ fb-js/fb_script.js
@@ -101,9 +101,9 @@ async function fb_changeGeoHighscoreTable() {
     //If score is there add it
     for (let i = 0; i < 5; i++) {
         if (userArrayGeo[i] != null) {
-            geoTable += '<tr><td>' + '<img class="img" src="' + userPhotoGeo[i] + '"</img>' + '</td><td>' + (i + 1) + ': ' + userArrayGeo[i] + '</td><td>' + (i + 1) + ': ' + userScoreGeo[i] + '</td></tr>';
+            geoTable += '<tr><td>' + '<img class="img" src="' + userPhotoGeo[i] + '"</img>' + '</td><td> ' + (i + 1) + ': ' + userArrayGeo[i] + '</td><td>' + (i + 1) + ': ' + userScoreGeo[i] + '</td></tr>';
         } else {
-            geoTable += '<tr><td>Empty</td><td>' + (i + 1) + ': ' + 'Empty' + '</td><td>' + (i + 1) + ': ' + 'Empty' + '</td></tr>';
+            geoTable += '<tr><td>Empty</td><td>' + (i + 1) + ': ' + 'Empty' + '</td><td> ' + (i + 1) + ': ' + 'Empty' + '</td></tr>';
         }
     }
     //Finish the table
@@ -153,9 +153,9 @@ async function fb_changeGameHighscoreTable() {
     //If score is there add it
     for (let i = 0; i < 5; i++) {
         if (userArrayGame[i] != null) {
-            gameTable += '<tr><td>' + '<img class="img" src="' + userPhotoGame[i] + '"</img>' + '</td><td>' + (i + 1) + ': ' + userArrayGame[i] + '</td><td>' + (i + 1) + ': ' + userScoreGame[i] + '</td></tr>';
+            gameTable += '<tr><td>' + '<img class="img" src="' + userPhotoGame[i] + '"</img>' + '</td><td> ' + (i + 1) + ': ' + userArrayGame[i] + '</td><td>' + (i + 1) + ': ' + userScoreGame[i] + '</td></tr>';
         } else {
-            gameTable += '<tr><td>Empty</td><td>' + (i + 1) + ': ' + 'Empty' + '</td><td>' + (i + 1) + ': ' + 'Empty' + '</td></tr>';
+            gameTable += '<tr><td>Empty</td><td>' + (i + 1) + ': ' + 'Empty' + '</td><td> ' + (i + 1) + ': ' + 'Empty' + '</td></tr>';
         }
     }
     //Finish the table
